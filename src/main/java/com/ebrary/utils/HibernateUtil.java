0d09f42d279ff318072633a4ae0d25138e0769f6
heIIoad <adrian.stepnik1997@gmail.com>
2021-05-13 19:27:43 +0200

    Hibernate configuration and util class

---
+++ src/main/java/com/ebrary/utils/HibernateUtil.java
@@ -0,0 +1,28 @@
+package com.ebrary.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static StandardServiceRegistry serviceRegistry;
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory(){
+        if (sessionFactory == null){
+            try{
+                Configuration conf = new Configuration().configure();
+                serviceRegistry = new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
+                sessionFactory = conf.buildSessionFactory(serviceRegistry);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+                if (serviceRegistry != null){
+                    StandardServiceRegistryBuilder.destroy(serviceRegistry);
+                }
+            }
+        }
+        return sessionFactory;
+    }
+}
