49e791977e8d95fa258bf14394a5e123622c4759
heIIoad <adrian.stepnik1997@gmail.com>
2021-05-14 22:03:22 +0200

    Blueprint of books controller

--- src/main/java/com/elibrary/controller/BookController.java
+++ src/main/java/com/elibrary/controller/BookController.java
@@ -1,17 +1,46 @@
 package com.elibrary.controller;
 
+import com.elibrary.entity.Book;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("books")
 @RestController
 public class BookController {
 
     @GetMapping
-    public String getBook(){
-        return "book";
+    public List<Book> getBooks(){
+        List<Book> books = new ArrayList<>();
+        books.add(new Book());
+        return books;
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book){
+        return new Book();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable Long id){
+        return new Book();
+    }
+
+    @PostMapping("/{id}")
+    public Book updateBook(@PathVariable Long id){
+        return new Book();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id){
+
     }
 
 }
